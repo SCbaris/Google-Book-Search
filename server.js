9b9237cc19cec127a51602f63d48ffaf7eea8a2c
unknown <baris.sahin.can@gmail.com>
2019-08-01 16:29:32 -0400

    heroku deployment 4, app didn't see database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path")
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise
 const routes = require("./routes");
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "public")))
 
 /*if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -23,6 +23,10 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://SCbariss:can123456@ds247637.mlab.com:47637/heroku_g8vx3r7k");
 // Start the API server
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
+});
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
