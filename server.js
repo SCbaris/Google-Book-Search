a03a7c453da62c1546ae03d1148cbe5284c273b7
unknown <baris.sahin.can@gmail.com>
2019-08-01 15:53:38 -0400

    Heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const mongoose = require("mongoose");
+mongoose.Promise = global.Promise
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,8 +17,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://SCbaris:cancan123.@ds247637.mlab.com:47637/heroku_g8vx3r7k");
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
