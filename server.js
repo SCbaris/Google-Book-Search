4cabeb0ea8c86790adc0236d79d317746e256247
unknown <baris.sahin.can@gmail.com>
2019-08-01 16:04:33 -0400

    heroku deployment 2, app didn't see database

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(routes);
 
 // Connect to the Mongo DB
 //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://SCbaris:cancan123.@ds247637.mlab.com:47637/heroku_g8vx3r7k");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://SCbariss:can123456@ds247637.mlab.com:47637/heroku_g8vx3r7k");
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
